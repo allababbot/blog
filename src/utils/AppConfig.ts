e204670c943369f9cee06f2be5bb051498352e06
allababbot <arisman.7403@gmail.com>
2025-12-18 13:06:46 +0700

    membersihkan template

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-	site_name: 'AstroPress',
-	title: 'AstroPress | Astro and Tailwind CSS',
-	description: 'Boilerplate built with Astro and Tailwind CSS',
-	author: 'Nicolas',
-	locale_region: 'de-en',
+	site_name: 'My Blog',
+	title: 'My Blog | Personal Website',
+	description: 'A personal blog built with Astro and Tailwind CSS',
+	author: 'Admin',
+	locale_region: 'en-US',
 	locale: 'en'
 };
